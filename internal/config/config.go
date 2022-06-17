bae3b8860b67f1e3de7d525c15137865b961a9c7
Григорий Ботаев <botaevgs@gmail.com>
2022-06-17 16:26:46 +0300

    increment 4.17

--- internal/config/config.go
+++ internal/config/config.go
@@ -7,9 +7,9 @@ import (
 )
 
 type Config struct {
-	ServerAddress   string `env:"SERVER_ADDRESS" envDefault:":8080"`
-	BaseURL         string `env:"BASE_URL" envDefault:"http://localhost:8080/"`
-	FileStoragePath string `env:"FILE_STORAGE_PATH" envDefault:"shortlist.txt"`
+	ServerAddress   string `env:"SERVER_ADDRESS"`    // envDefault:":8080"`
+	BaseURL         string `env:"BASE_URL"`          // envDefault:"http://localhost:8080/"`
+	FileStoragePath string `env:"FILE_STORAGE_PATH"` // envDefault:"shortlist.txt"`
 }
 
 func GetConfig() Config {
